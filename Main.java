a1e28df5e5ad1f32ffdbda975623ff0e33f8edde
nicklanetv <nicklanetv@gmail.com>
2013-10-31 11:38:25 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+package itdoperations;
+
+import itdoperations.operations;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nick
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       operations form = new operations();
+        form.setVisible(true);
+        
+    }
+    
+}
